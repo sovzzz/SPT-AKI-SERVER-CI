20d524b67eee5a602e571fb38a8a852d6b2fe86c
sovzzz <60016107+sovzzz@users.noreply.github.com>
2023-11-05 11:01:10 +0800

    Update BuildServer.ps1

--- BuildServer.ps1
+++ BuildServer.ps1
@@ -27,6 +27,8 @@ foreach ($branch in $branches) {
     npm install
     npm run build:debug
     # 获取当前日期，格式为yyyy-MM-dd
+    # 进入build文件夹
+    cd build
     $date = Get-Date -Format "yyyy-MM-dd"
     $AkiMeta = (Get-Content ./build/Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
     # 拼接压缩包的名称，包含分支名和当前日期
@@ -34,9 +36,10 @@ foreach ($branch in $branches) {
     # 清理分支名，移除或替换特殊字符
     $cleanBranch = $branch -replace '[\\/:"*?<>|]', '_'
     # 拼接压缩包的名称，包含分支名和当前日期
-    $zipName = "Aki.Server-v.$($AkiMeta.akiVersion)-b.$cleanBranch-$date.zip"
+    $zipName = "Aki.Server-b.$cleanBranch-v.$($AkiMeta.akiVersion)-$date.zip"
     # 压缩build文件夹，命名为压缩包的名称，放在上上级目录的distr文件夹里
     Compress-Archive -Path build -DestinationPath ..\..\distr\$zipName
     # 返回上一级目录
+    cd ..
     cd ..
 }
