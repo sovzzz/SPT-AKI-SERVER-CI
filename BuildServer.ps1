f62cc2a7ec7b0b363c0cbeee1ddc6c561d1e52e0
sovzzz <60016107+sovzzz@users.noreply.github.com>
2023-11-05 10:29:36 +0800

    Update BuildServer.ps1

--- BuildServer.ps1
+++ BuildServer.ps1
@@ -5,7 +5,9 @@ cd Server
 # 获取所有分支的信息
 git fetch --all
 # 获取所有分支的名称
-$branches = git branch -a | Select-String -Pattern "remotes/origin" | ForEach-Object { $_.Line.Trim() -replace "remotes/origin/", "" }
+#$branches = git branch -a | Select-String -Pattern "remotes/origin" | ForEach-Object { $_.Line.Trim() -replace "remotes/origin/", "" }
+$branches = git branch -a | Select-String -Pattern "remotes/origin" -NotMatch "HEAD ->" | ForEach-Object { $_.Line.Trim() -replace "remotes/origin/", "" }
+
 # 遍历每个分支
 foreach ($branch in $branches) {
     # 切换到当前分支
@@ -22,7 +24,7 @@ foreach ($branch in $branches) {
     $date = Get-Date -Format "yyyy-MM-dd"
     $AkiMeta = (Get-Content ./build/Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
     # 拼接压缩包的名称，包含分支名和当前日期
-    $zipName = "Aki.Server-v.$($akimeta.akiVersion)-b.$branch-$date.zip"
+    $zipName = "Aki.Server-v.$($Akimeta.akiVersion)-b.$branch-$date.zip"
     # 压缩build文件夹，命名为压缩包的名称，放在上上级目录的distr文件夹里
     Compress-Archive -Path build -DestinationPath ..\..\distr\$zipName
     # 返回上一级目录
