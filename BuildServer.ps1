4bb600b1df88601a49ab958e96343dac86457c8b
sovzzz <60016107+sovzzz@users.noreply.github.com>
2023-11-06 15:22:38 +0800

    Update BuildServer.ps1

--- BuildServer.ps1
+++ BuildServer.ps1
@@ -29,7 +29,7 @@ foreach ($branch in $branches) {
     # 获取当前日期，格式为yyyy-MM-dd
     # 进入build文件夹
     cd build
-    $date = Get-Date -Format "yyyy-MM-dd"
+    $date = Get-Date -Format "yyyy.MM.dd"
     $AkiMeta = (Get-Content ./Aki_Data/Server/configs/core.json |  ConvertFrom-Json -AsHashtable)
     # 拼接压缩包的名称，包含分支名和当前日期
     #$zipName = "Aki.Server-v.$($Akimeta.akiVersion)-b.$branch-$date.zip"
