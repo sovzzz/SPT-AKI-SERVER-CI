a74bf092c6199f380c2cb7fc5571c37063b9bd20
sovzzz <60016107+sovzzz@users.noreply.github.com>
2024-05-21 15:01:10 +0800

    Update BuildLauncher.ps1

--- BuildLauncher.ps1
+++ BuildLauncher.ps1
@@ -18,8 +18,13 @@ foreach ($branch in $branches) {
     #Launcher/project/Build
     $date = Get-Date -Format "yyyy.MM.dd"
     $cleanBranch = $branch -replace '[\\/:"*?<>|]', '_'
-    $zipName = "Aki.Launcher-b.$cleanBranch-d.$date.zip"
-    Compress-Archive -Path * -DestinationPath ..\..\..\dist\$zipName
+    #$zipName = "Aki.Launcher-b.$cleanBranch-d.$date.zip"
+    #Compress-Archive -Path * -DestinationPath ..\..\..\dist\$zipName
+    $zipName = "Aki.Server-b.$cleanBranch-v.$($AkiMeta.akiVersion)-t.$($AkiMeta.compatibleTarkovVersion)-d.$date.zip"
+    $sevenZipName = "Aki.Launcher-b.$cleanBranch-d.$date.7z"
+    # 压缩build文件夹，命名为压缩包的名称，放在上上级目录的distr文件夹里
+    #Compress-Archive -Path * -DestinationPath ..\..\..\distr\$zipName
+    7z a -t7z "..\..\..\distr\$sevenZipName" * -mx=9
     cd ..
     cd ..
 }
